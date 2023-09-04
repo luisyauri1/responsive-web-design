2b9eaa75c31f7d9990288d933d6f16e4f3f533b0
Luis Yauri <luis.yauri159@gmail.com>
2023-09-04 16:39:02 -0500

    Update menu

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const replace = require('gulp-replace');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 
@@ -6,6 +7,7 @@ function compileSass() {
   return gulp
     .src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(replace('src/', '../'))
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(browserSync.stream());
 }
